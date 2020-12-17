f8cb140f83eafd3b82c04234c06d4eddaafa6678
unknown <jackmao2007@outlook.com>
2020-12-17 18:42:21 -0500

    repull all

---
+++ pub/Blocker.js
@@ -0,0 +1,386 @@
+function Blocker(selector) {
+    let self = {}
+    self.selector = selector
+    self.element = document.querySelector(self.selector)
+    self.replacement = self.element.cloneNode(true)
+    self.showAction = () => self.replacement.dispatchEvent(self.showEvent)
+    self.showEvent = new CustomEvent('blockerjsShowEvent')
+    self.afterEvent = new CustomEvent('blockerjsAfterEvent')
+
+    self.showMethod = () => self.replacement.parentElement.replaceChild(self.element, self.replacement)
+    self.afterEffect = () => {
+        return
+    }
+
+    self.hide = function(triggerEvent, triggerEventOptions={}){
+        // need custom event to trigger showing
+        self.replacement = document.createElement("div")
+        let replacement = self.replacement
+        replacement.id = self.element.id
+        replacement.style = self.element.style
+
+        this.setupTriggerEvent(triggerEvent, triggerEventOptions)
+        replacement.addEventListener(triggerEvent, (event) => {
+            replacement.parentElement.replaceChild(self.element, replacement)
+        })
+        self.element.parentElement.replaceChild(replacement, self.element)
+        return self
+    }
+
+    self.block = function(color="none", triggerEvent="click", triggerEventOptions={}){
+        const width = self.element.offsetWidth
+        const height = self.element.offsetHeight
+        self.replacement = document.createElement("div")
+        let replacement = self.replacement
+        replacement.id = self.element.id
+        replacement.style = self.element.style + `;min-height: ${height}px; min-width: ${width}px; background-color: ${color};`
+        this.setupTriggerEvent(triggerEvent, triggerEventOptions)
+        replacement.addEventListener(triggerEvent, (event) => {
+            self.showAction()
+        })
+        replacement.addEventListener('blockerjsShowEvent', (event) => {
+            self.showMethod()
+        })
+        self.element.addEventListener('blockerjsAfterEvent', (event) => {
+            self.afterEffect()
+        })
+        self.element.parentElement.replaceChild(replacement, self.element)
+        return self
+
+    }
+
+    self.blur = function(blurStrength, triggerEvent="click", triggerEventOptions={}){
+        const width = self.element.offsetWidth
+        const height = self.element.offsetHeight
+        let replacement = self.replacement
+        replacement.style = self.element.style + `; -webkit-filter: blur(${blurStrength}px);
+                                                    -moz-filter: blur(${blurStrength}px);
+                                                    -o-filter: blur(${blurStrength}px);
+                                                    -ms-filter: blur(${blurStrength});filter: blur(${blurStrength}px);`
+        this.setupTriggerEvent(triggerEvent, triggerEventOptions)
+        replacement.addEventListener(triggerEvent, (event) => {
+            self.showAction()
+        })
+        replacement.addEventListener('blockerjsShowEvent', (event) => {
+            self.showMethod()
+            
+        })
+        self.element.addEventListener('blockerjsAfterEvent', (event) => {
+            self.afterEffect()
+        })
+        self.element.parentElement.replaceChild(replacement, self.element)
+        return self
+    }
+
+    // setupTriggerEvent
+    self.setupTriggerEvent = function(triggerEvent, triggerEventOptions) {
+        if (triggerEvent === "blockerjsScroll") {
+            const scrollEvent = new CustomEvent('blockerjsScroll')
+            window.onscroll = function() {myFunction()};
+            const scrollTop = triggerEventOptions.scrollTop
+            function myFunction() {
+                if (document.body.scrollTop > scrollTop || document.documentElement.scrollTop > scrollTop) {
+                    self.replacement.dispatchEvent(scrollEvent)
+                }
+            }
+        } else if (triggerEvent === "blockerjsHold") {
+            let timer = 0
+            const holdEvent = new CustomEvent('blockerjsHold')
+            let counter = ''
+            self.replacement.addEventListener("mousedown", (e) => {
+                counter = setInterval(() => {
+                    timer += 500
+                    if (timer >= triggerEventOptions.holdDuration){
+                        clearInterval(counter)
+                        self.replacement.dispatchEvent(holdEvent)
+                    }
+                }, 500)
+            })
+            self.replacement.addEventListener("mouseup", (e) => {
+                timer = 0
+                clearInterval(counter)
+            })
+        } else if (triggerEvent === "blockerjsMultiClick") {
+            const multiClickEvent = new CustomEvent('blockerjsMultiClick')
+            let totalClicks = 0
+            self.replacement.addEventListener("click", (e) => {
+                totalClicks += 1
+                if (totalClicks >= triggerEventOptions.multiClicks){
+                    totalClicks = 0
+                    self.replacement.dispatchEvent(multiClickEvent)
+                }
+            })
+        }
+    }
+
+    self.setShowAction = function(config){
+        if (config.mode === "password"){
+            self.showAction = () => {
+                if (self.replacement.parentElement.querySelectorAll(":scope > .passwordShowActionContainer").length === 0){
+                    let cont = document.createElement("div")
+                    let btn = document.createElement("button")
+                    let inpt = document.createElement("input")
+                    cont.className = "passwordShowActionContainer"
+                    const repPos = self.replacement.getBoundingClientRect()
+                    cont.style = `; position: absolute; left:${repPos.x + document.documentElement.scrollLeft+ repPos.width/2}px;
+                                    top:${repPos.y + document.documentElement.scrollTop + repPos.height/2}px; transform: translate(-50%, -50%);
+                                    max-width: ${repPos.width}; max-height: ${repPos.height}`
+                    btn.innerHTML = "confirm"
+                    cont.appendChild(inpt)
+                    cont.appendChild(btn)
+                    inpt.placeholder = "enter the password"
+                    const checkVal = (event) => {
+                        if (inpt.value == config.password){
+                            self.replacement.dispatchEvent(self.showEvent)
+                            cont.parentElement.removeChild(cont)
+                        }else{
+                            cont.parentElement.removeChild(cont)
+                        }
+                    }
+                    btn.onclick = checkVal
+                    self.replacement.parentElement.appendChild(cont) 
+                }
+            }
+        } else if (config.mode === "puzzle"){
+            self.showAction = () => {
+                const size = config.size ? config.size : 4
+                if (self.puzzleSvg){
+                    return
+                }
+
+                let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                self.puzzleSvg = svg
+                svg.setAttribute("width", 100);
+                svg.setAttribute("height", 100);
+                svg.setAttribute("viewBox", [0, 0, 100 * size, 100 * size].join(" "));
+                const repPos = self.replacement.getBoundingClientRect()
+                svg.style = `; position: absolute; left:${repPos.x + document.documentElement.scrollLeft+ repPos.width/2}px;
+                top:${repPos.y + document.documentElement.scrollTop + repPos.height/2}px; transform: translate(-50%, -50%);
+                border: 1px solid black`
+
+                this.replacement.parentElement.appendChild(svg)
+
+                let map = []
+                for (let i = 0; i < size; i++) {
+                    map.push([])
+                    for (let j = 0; j < size; j++){
+                        console.log(i, j)
+                        map[i].push(false)
+                        let g = document.createElementNS("http://www.w3.org/2000/svg", 'g')
+                        let rec = document.createElementNS("http://www.w3.org/2000/svg", 'rect')
+                        g.setAttribute("transform", ["translate(", j*100, ",", i*100, ")"].join(""))
+                        g.appendChild(rec)
+                        rec.setAttribute("width", 100)
+                        rec.setAttribute("height", 100)
+                        rec.setAttribute("fill", "white")
+                        rec.style = "; stroke:black; stroke-width:1;"
+                        svg.appendChild(g)
+                        rec.addEventListener("click", () => {
+                            map[i][j] = !map[i][j]
+                            if (map[i][j]){
+                                rec.setAttribute("fill", config.color)
+                            } else {
+                                rec.setAttribute("fill", "white")
+                            }
+                            console.log(JSON.stringify(map))
+                            console.log(JSON.stringify(config.trueMap))
+                            if (JSON.stringify(map) == JSON.stringify(config.trueMap)){
+                                self.replacement.dispatchEvent(self.showEvent)
+                                svg.parentElement.removeChild(svg)
+                                self.svg = null
+                            }
+                        })
+                    }
+
+                }
+
+            }
+        }
+        return self
+    }
+
+
+    // set showmethod
+
+    self.setShowMethod = function (config) {
+        if (config.mode === "scratch"){
+            self.showMethod = () => {
+                let isDrawing, lastPoint
+                const percentReveil = config.percentReveil ? config.percentReveil : 50
+                const scratchSize = config.scratchSize ? config.scratchSize : 25
+                const canvas = document.createElement('canvas')
+                const ctx = canvas.getContext('2d')
+                const repPos = self.replacement.getBoundingClientRect()
+                canvas.style = `; position: absolute; left:${repPos.x + document.documentElement.scrollLeft}px;
+                                    top:${repPos.y + document.documentElement.scrollTop}px;}`
+                canvas.height = repPos.height
+                canvas.width = repPos.width
+            
+                ctx.beginPath()
+                ctx.rect(0, 0, canvas.width, canvas.height)
+                ctx.fillStyle = config.color
+                ctx.fill()
+                ctx.textBaseline = 'middle';
+                ctx.textAlign = "center";
+                ctx.font = "30px Comic Sans MS";
+                ctx.fillStyle = "white";
+                ctx.fillText("Scratch", canvas.width/2, canvas.height/2);
+                
+                canvas.addEventListener('mousedown', handleMouseDown, false)
+                canvas.addEventListener('mousemove', handleMouseMove, false)
+                canvas.addEventListener('mouseup', handleMouseUp, false)
+
+                self.replacement.parentElement.appendChild(canvas) 
+                self.replacement.parentElement.replaceChild(self.element, self.replacement)
+                
+                function getBrushPos(e, canvas) {
+                    const xRef = e.clientX
+                    const yRef = e.clientY
+                    var canvasRect = canvas.getBoundingClientRect()
+                    return {
+                    x: Math.floor((xRef-canvasRect.left)/(canvasRect.right-canvasRect.left)*canvas.width),
+                    y: Math.floor((yRef-canvasRect.top)/(canvasRect.bottom-canvasRect.top)*canvas.height)
+                    }
+                }
+                
+                
+                function handlePercentage() {
+                    const pixels   = ctx.getImageData(0, 0, canvas.width, canvas.height)
+                    const pdata    = pixels.data
+                    
+                    let count    = 0;
+                
+                    for(var i = count = 0; i < pdata.length; i += 1) {
+                        if (parseInt(pdata[i]) === 0) {
+                            count++;
+                        }
+                    }
+                    if (count / pdata.length * 100 > percentReveil) {
+                        canvas.parentNode.removeChild(canvas)
+                        self.element.dispatchEvent(self.afterEvent)
+                    }
+                }
+                
+                function handleMouseDown(e) {
+                isDrawing = true;
+                lastPoint = getBrushPos(e, canvas)
+                }
+
+                function handleMouseMove(e) {
+                    if (isDrawing) { 
+                        e.preventDefault()
+                
+                        var currentPoint = getBrushPos(e, canvas),
+                            x, y;
+                        
+                        x = lastPoint.x 
+                        y = lastPoint.y 
+                        ctx.beginPath();
+                        ctx.arc(x, y, scratchSize, 0, 2*Math.PI, true)
+                        ctx.fillStyle = '#000'
+                        ctx.globalCompositeOperation = "destination-out"
+                        ctx.fill()
+                        
+                        lastPoint = currentPoint
+                        handlePercentage()
+                    }
+                }
+                    
+                function handleMouseUp(e) {
+                        isDrawing = false
+                }
+            }
+
+        } else if (config.mode === 'fade') {
+            self.showMethod = () => {
+                let rep = document.createElement("div")
+                const repPos = self.replacement.getBoundingClientRect()
+                rep.style = self.element.style + `; position: absolute; left:${repPos.x + document.documentElement.scrollLeft}px;
+                top:${repPos.y + document.documentElement.scrollTop}px;}; height:${repPos.height}px; width:${repPos.width}px; background-color: ${config.color};`
+
+                self.replacement.parentElement.appendChild(rep)
+                self.replacement.parentElement.replaceChild(self.element, self.replacement)
+    
+                let op = 1;  // initial opacity
+                let timer = setInterval(function () {
+                    if (op <= 0.1){
+                        clearInterval(timer);
+                        rep.parentElement.removeChild(rep)
+                        self.element.dispatchEvent(self.afterEvent)
+                    }
+                    rep.style.opacity = op;
+                    rep.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                    op -= op * 0.1;
+                }, 50);
+            }
+        } else if (config.mode === "slide") {
+            self.showMethod = () => {
+                let rep = document.createElement("div")
+                const speed = config.speed ? config.speed : 10
+                const repPos = self.replacement.getBoundingClientRect()
+                rep.style = self.element.style + `; position: absolute; left:${repPos.x + document.documentElement.scrollLeft}px;
+                top:${repPos.y + document.documentElement.scrollTop}px;}; height:${repPos.height}px; width:${repPos.width}px; background-color: ${config.color};`
+
+                self.replacement.parentElement.appendChild(rep)
+                self.replacement.parentElement.replaceChild(self.element, self.replacement)
+
+
+                let pos = parseInt(rep.style.left);  // initial position
+                const end = pos + parseInt(rep.style.width)
+
+                let anim = setInterval(function () {
+                    if (pos >= end){
+                        clearInterval(anim);
+                        rep.parentElement.removeChild(rep)
+                        self.element.dispatchEvent(self.afterEvent)
+                    }
+                    rep.style.left = pos + "px";
+                    pos = pos + Math.ceil(speed * parseInt(rep.style.width)/(end - pos))
+                }, 25);
+            }
+        }
+        return self
+    }
+
+    // set afterEffect
+    self.setAfterEffect = function (config){
+        if (config.mode === "recover" ){
+            const time = config.time
+            self.afterEffect = () => {
+                setTimeout(() => {self.element.parentElement.replaceChild(self.replacement, self.element)}, time)
+            }
+        }else if (config.mode === "tremble"){
+            const time = config.time
+            let mag = config.trembleMagnitude ? config.trembleMagnitude : 10
+            self.afterEffect = () => {
+                let runTremb = true
+                setTimeout(() => {
+                    runTremb = false
+                }, time);
+
+                const tremb = () => {                  
+                    const randomInt = (min, max) => {
+                      return Math.floor(Math.random() * (max - min + 1)) + min;
+                    };
+                    if (runTremb) {
+                
+                    const randomX = randomInt(-mag, mag)
+                    const randomY = randomInt(-mag, mag)
+                
+                    self.element.style.transform = 'translate(' + randomX + 'px, ' + randomY + 'px)'
+                
+                    requestAnimationFrame(tremb)
+                    } else {
+                    self.element.style.transform = 'translate(' + 0 + ', ' + 0 + ')'
+                    }
+                }
+
+                tremb()
+            }
+            
+        }
+        return self
+    }
+
+    return self
+}
---
+++ pub/example.js
@@ -0,0 +1,24 @@
+// setup
+const triggerShow = new CustomEvent('show1')
+document.querySelector('#demo-trigger-1').addEventListener("click", (event) => {
+  document.querySelector('#hide-demo').dispatchEvent(triggerShow)
+  })
+// end setup
+function showContent() {
+  return true
+}
+
+Blocker("#hide-demo").hide('show1')
+let blockDemo = Blocker("#block-demo").block("orange", "blockerjsHold", {holdDuration: 2000})
+blockDemo.setAfterEffect({mode: "tremble", time: 2000})
+blockDemo.setShowMethod({mode: "scratch", color: "orange", percentReveil: 70, scratchSize: 10})
+let blockDemo2 = Blocker("#block-demo2").block("orange")
+blockDemo2.setAfterEffect({mode: "tremble", time: 2000})
+blockDemo2.setShowMethod({mode: 'slide', color: 'orange'})
+blockDemo2.setShowAction({mode: "puzzle",color:"orange", trueMap: [[true, false,false,false],
+                                                                  [true, false,false,false],
+                                                                  [false, true,false,false],
+                                                                  [false, false,true,false]]})
+Blocker("#blur-demo").blur(2, "blockerjsScroll", {scrollTop: 100})
+let passwordDemo = Blocker("#password-demo").setShowAction({mode: "password", password:"123123"}).block("orange", "blockerjsMultiClick", {multiClicks: 5})
+passwordDemo.setAfterEffect({mode:"recover", time: 2000})
